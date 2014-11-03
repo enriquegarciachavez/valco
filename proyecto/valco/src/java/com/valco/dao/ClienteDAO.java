80d9a19cacb28b7ea36f091dc8dac4a821ffebdc
enriquegarciachavez <enrique.garcia.chavez@hotmail.com>
2014-11-02 19:25:53 -0700

    se crean clases pojo y cliente dao

---
+++ proyecto/valco/src/java/com/valco/dao/ClienteDAO.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.valco.dao;
+
+import com.valco.HibernateUtil;
+import com.valco.pojo.Clientes;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Enrique
+ */
+public class ClienteDAO {
+
+    public void insertarCliente(Clientes cliente) throws Exception {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.save(cliente);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                try {
+                    tx.rollback();
+                } catch (HibernateException he) {
+                    throw new Exception("Ocurrió un error al registrar el cliente.");
+                }
+            }
+            throw new Exception("Ocurrió un error al registrar el cliente.");
+        } finally {
+            try {
+                session.close();
+            } catch (HibernateException he) {
+                throw new Exception("Ocurrió un error al registrar el cliente.");
+            }
+        }
+    }
+     public void actualizarCliente(Clientes cliente) throws Exception {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.update(cliente);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                try {
+                    tx.rollback();
+                } catch (HibernateException he) {
+                    throw new Exception("Ocurrió un error al modificar el cliente.");
+                }
+            }
+            throw new Exception("Ocurrió un error al modificar el cliente.");
+        } finally {
+            try {
+                session.close();
+            } catch (HibernateException he) {
+                throw new Exception("Ocurrió un error al modificar el cliente.");
+            }
+        }
+    }
+      public void borrarCliente(Clientes cliente) throws Exception {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.delete(cliente);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                try {
+                    tx.rollback();
+                } catch (HibernateException he) {
+                    throw new Exception("Ocurrió un error al borrar el cliente.");
+                }
+            }
+            throw new Exception("Ocurrió un error al borrar el cliente.");
+        } finally {
+            try {
+                session.close();
+            } catch (HibernateException he) {
+                throw new Exception("Ocurrió un error al borrar el cliente.");
+            }
+        }
+    }
+      public List<Clientes> getCliente() throws Exception {
+          Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+          Transaction tx = null;
+          List<Clientes> clientes = new ArrayList<Clientes>();
+          try {
+              tx = session.beginTransaction();
+              Query q = session.createQuery("FROM Clientes");
+              clientes = (List<Clientes>) q.list();
+              return clientes;
+
+          } catch (HibernateException he) {
+              throw new Exception("Ocurrió un error al consultar los clientes.");
+
+          } finally {
+              try {
+                  session.close();
+              } catch (HibernateException he) {
+                  throw new Exception("Ocurrió un error al consultar los clientes.");
+              }
+        }
+    }
+}
