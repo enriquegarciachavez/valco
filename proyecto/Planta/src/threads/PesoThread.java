d6cbe9143c04d243d962cea121cb48c9524810c4
enriquegarciachavez <enrique.garcia.chavez@hotmail.com>
2015-07-06 19:21:13 -0600

    thread para leer la bascula

---
+++ proyecto/Planta/src/threads/PesoThread.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threads;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Karla
+ */
+public class PesoThread implements Runnable {
+    JLabel pesoLbl;
+
+    @Override
+    public void run() {
+
+    }
+    
+}
