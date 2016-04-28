c6903844adbb9970b2032db303cdda1032e7a707
enriquegarciachavez <enrique.garcia.chavez@hotmail.com>
2016-04-27 19:55:58 -0600

    conflictos resueltos

--- proyecto/Planta/src/dao/ProductoDAO.java
+++ proyecto/Planta/src/dao/ProductoDAO.java
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -407,7 +407,7 @@ public class ProductoDAO {
           //session.clear();
           Transaction tx = null;
           List<ProductosInventario> productos = new ArrayList<ProductosInventario>();
-          Transaction tx = null;
+
           try {
               tx = session.beginTransaction();
               //session.clear();
